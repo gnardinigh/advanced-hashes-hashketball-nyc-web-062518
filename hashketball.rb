14817a413fa63bdc37fe8af9c2f675a614443be7
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 18:41:39 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
   the_hash={
         :home => {
               :team_name => "Brooklyn Nets",
-              :colors => "Black, White",
+              :colors => ["Black","White"],
               :players => {
                     "Alan Anderson" => {
                         :number => "0",
