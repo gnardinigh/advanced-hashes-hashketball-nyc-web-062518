fe75ffc9d2ce7b415c0c1264fbdbd2dad08bd56f
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 20:09:44 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,7 +189,7 @@ def player_numbers(team_name)
   
   game_hash.each do |location,team_stats|
     if team_stats[:team_name] == team_name
-      puts team_stats
+      team_stats.each do |data_type,data|
     end  
   end
 end
