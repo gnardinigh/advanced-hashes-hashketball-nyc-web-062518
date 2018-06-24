2341745f8e5c51175fe52ddf2b75a869a0e74bff
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 19:45:17 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,7 +163,7 @@ end
 
 def team_colors(team_name)
   game_hash.each do |location,team_data|
-    team_data.each do |data_type,data|
-      
+    team_data.each do |data_type,datax|
+      if 
   end
 end
